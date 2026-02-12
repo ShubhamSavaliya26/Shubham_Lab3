ce48f036477bf43d7a4527a6777dfda5fee1038a
Shubham Savaliya <shubhamsavaliya2004@gmail.com>
2026-02-12 16:38:34 -0500

    Initial commit - Shubham Lab 3 ASP.NET Core Web API

---
+++ Shubham_Lab3/Program.cs
@@ -0,0 +1,27 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapGet("/hello", () => "Hello from your Azure API running .NET 9");
+
+app.MapControllers();
+
+app.Run();
